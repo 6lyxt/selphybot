b43e237a0e611ba57d04b4ac0bb429f824e79cf7
Johannes M. Dietze <58228327+6lyxt@users.noreply.github.com>
2020-08-19 01:36:29 +0200

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -6,6 +6,15 @@ app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
+app.get('/bot/' || '/bot', function(req, res) {
+  res.render('bot.ejs')
+})
+
+app.get('/invite', function(req, res) {
+  res.redirect('https://discord.com/api/oauth2/authorize?client_id=744710150782189611&permissions=8&scope=bot')
+})
+
+
 app.listen(80, function() {
   console.log("running on port 80")
 })
