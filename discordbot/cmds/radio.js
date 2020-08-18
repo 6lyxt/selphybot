fc0e9d7033e7cc103108e74d85682653fa2f2aaf
Johannes M. Dietze <58228327+6lyxt@users.noreply.github.com>
2020-08-18 15:46:05 +0200

    did an oopsie

--- discordbot/cmds/radio.js
+++ discordbot/cmds/radio.js
@@ -20,7 +20,7 @@ module.exports = {
           utils.throwEmbed(msg, "Radio", "Momentan läuft: \n" + title, "#c96363", "https://camo.githubusercontent.com/323e46a74461fd6c201cf91da6a8d2cbcca92297/68747470733a2f2f7669676e657474652e77696b69612e6e6f636f6f6b69652e6e65742f6e69636f646f75676173696e676572732f696d616765732f312f31382f53656c7068792e706e672f7265766973696f6e2f6c61746573743f63623d3230313830393037303531353533")
           const stream = ytdl(url, { filter: 'audioonly' });
           const dispatcher = connection.play(stream);
-          dispatcher.on('finish', () => radio(msg);
+          dispatcher.on('finish', () => radio(msg));
       } else {
         console.log("rip server")
       }
