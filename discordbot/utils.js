10b1cb363976f79b55469f8fe2f3b3b1fc09649f
Johannes M. Dietze <58228327+6lyxt@users.noreply.github.com>
2020-08-19 01:29:43 +0200

    added title urls + fixed twitch

--- discordbot/utils.js
+++ discordbot/utils.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 
 module.exports = {
-      throwEmbed: async function throwEmbed(msg, title, description, color ,thumbnail){
+      throwEmbed: async function throwEmbed(msg, title, description, color ,thumbnail, url){
           const embed = new Discord.MessageEmbed()
           .setFooter("selphybot by lyxt#0666")
+          .setURL(url)
           .setColor(color)
           .setTitle(title)
           .setDescription(description)
