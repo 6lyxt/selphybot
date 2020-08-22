77d7c8041ec5abe9afae7e2b6d235c3446f9f6da
Johannes M. Dietze <58228327+6lyxt@users.noreply.github.com>
2020-08-23 01:38:14 +0200

    small adjustments

--- discordbot/cmds/radio.js
+++ discordbot/cmds/radio.js
@@ -19,11 +19,13 @@ module.exports = {
       ajax.send();
       ajax.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
+          if(!(this.responseText.toString().includes("error"))){
+
           let jsonfs = JSON.parse(this.responseText);
-          let url = jsonfs.link;
-          if(url != null || url != undefined){
+            let url = jsonfs.link;
             let title = jsonfs.title;
-            utils.throwEmbed(msg, "Radio", "Momentan läuft: \n" + title, "#c96363", "https://camo.githubusercontent.com/323e46a74461fd6c201cf91da6a8d2cbcca92297/68747470733a2f2f7669676e657474652e77696b69612e6e6f636f6f6b69652e6e65742f6e69636f646f75676173696e676572732f696d616765732f312f31382f53656c7068792e706e672f7265766973696f6e2f6c61746573743f63623d3230313830393037303531353533", url)
+            let thumb = jsonfs.thumbnail;
+            utils.throwEmbed(msg, "Radio", "Momentan läuft: \n" + title, "#c96363", thumb, url)
             const stream = ytdl(url, { filter: 'audioonly' });
             const dispatcher = connection.play(stream);
             dispatcher.on('finish', () => radio(msg));
@@ -31,7 +33,8 @@ module.exports = {
           msg.channel.send("> Oopsie! Anscheinend ist ein kleiner Fehler aufgetreten. Starte das Radio einfach mal neu, dann sollte alles wieder in Ordnung sein ^-^")
         }
     }
-  })
+  }
+})
 }
 }
 
